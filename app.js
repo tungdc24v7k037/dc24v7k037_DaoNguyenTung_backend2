61a72766fcf9e5d459488cd6bc5d0b28ab3433c6
Dao Nguyen Tung <tungdc24v7k037@gmail.com>
2026-04-28 16:02:46 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const express= require("express");
-const cors=require("cors");
-const app=express();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const contactsRouter = require("./app/routes/contact.route");
 app.use(cors());
 app.use(express.json());
-app.get("/", (req, res)=>{
-    res.json({message:"Welcome to contact book application."});
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to contact book application." });
 });
-module.exports= app;
+
+app.use("/api/contacts", contactsRouter);
+module.exports = app;
